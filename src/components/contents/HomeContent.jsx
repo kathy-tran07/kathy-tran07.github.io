c7ccb898a15cdc01c4285fea6e4a6d101b5f646a
Kathy Tran <113992934+kathy-tran07@users.noreply.github.com>
2025-10-21 11:44:10 -0400

    adjust css to fit screens better

--- src/components/contents/HomeContent.jsx
+++ src/components/contents/HomeContent.jsx
@@ -3,14 +3,16 @@ import Typewriter from 'typewriter-effect';
 
 const HomeContent = () => {
     return (
-        <div className="home_content">
-            <Typewriter
-                options={{
-                    strings: ['Kathy Tran—Technology and Media', 'Software Engineer & Filmmaker', 'Previously @ Amazon Web Services'],
-                    autoStart: true,
-                    loop: true,
-                }}
-            />
+        <div className='home_content_cont'>
+            <div className="home_content">
+                <Typewriter
+                    options={{
+                        strings: ['Kathy Tran—Technology and Media', 'Software Engineer & Filmmaker', 'Previously @ Amazon Web Services'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </div>
         </div>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [
     [react()],
   ],
-   base: '/https://kathy-tran07.github.io',
+   base: '/',
 })
